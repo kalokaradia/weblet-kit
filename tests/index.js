5d0a00bfd1bd48f61eb0e0ea109616debff0438c
Oasis Spensa <kalokaradia@gmail.com>
2025-09-10 21:46:16 +0700

    edit README.md with add the documentation

--- tests/index.js
+++ tests/index.js
@@ -1,25 +1,3 @@
-import wk from "../dist/weblet-kit.min.mjs";
+import { utils } from "https://unpkg.com/weblet-kit@1.0.0/dist/weblet-kit.min.mjs";
 
-const logScroll = wk.utils.throttle(() => {
-	console.log("Scrolled:", Date.now());
-}, 1000);
-
-window.addEventListener("scroll", logScroll);
-
-const search = wk.utils.debounce((query) => {
-	console.log("Searching:", query);
-}, 2000);
-
-search("h");
-search("he");
-search("hel");
-search("hell");
-search("hello");
-
-// Output (setelah 500ms terakhir): "Searching: hello"
-
-// Kalau kamu scroll terus, log muncul tiap 1 detik, bukan setiap pixel.
-
-const input = `<div class="bad">Oke & "quote"</div>`;
-console.log(wk.utils.escapeHTML(input));
-// output: "&lt;div class=&quot;bad&quot;&gt;Oke &amp; &quot;quote&quot;&lt;/div&gt;"
+console.log(utils.isStrongPassword("P@ssword123"));
